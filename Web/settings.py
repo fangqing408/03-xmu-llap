c53d8826f63e315f1befcfe7e4432d89bc17beac
fangqing408 <2463973240@qq.com>
2025-04-10 15:12:22 +0800

    xmu

--- Web/settings.py
+++ Web/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+STATIC_ROOT = BASE_BASE_DIR / 'allstatic'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
